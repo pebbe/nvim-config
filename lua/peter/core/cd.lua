6ceb36511c80f72b7da077fef2bae93859c4dc98
Peter Kleiweg <kleiweg@ziggo.nl>
2023-01-06 15:48:12 +0100

    cd.lua: config vanuit bestand lezen

--- lua/peter/core/cd.lua
+++ lua/peter/core/cd.lua
@@ -1,20 +1,14 @@
 local M = {}
 
-local data = {
-	--	{ 'home', '~' },
-	{ 'nvim', '~/.nvim/config/nvim' },
-	{ 'blog', '~/blog/content/posts/2023' },
-	{ 'lentis', '~/lentis/content/post/2023' },
-	{ 'werk', '~/werklog/2023' },
-  { 'go', '~/go/src/github.com'},
-	{ 'alpinograph', '~/web/urd/www/alpinograph' },
-}
-
 local cd = {}
 local keys = {}
-for _, value in pairs(data) do
-	table.insert(keys, value[1])
-	cd[value[1]] = value[2]
+
+for line in io.lines(vim.fn.stdpath('config') .. '/dirs') do
+	local _, _, key, value = string.find(line, '^%s*(.-)%s*|%s*(.-)%s*$')
+	if value ~= nil then
+		table.insert(keys, key)
+		cd[key] = value
+	end
 end
 
 M.cd = function()
@@ -23,6 +17,7 @@ M.cd = function()
 	}, function(choice)
 		if choice ~= nil then
 			vim.cmd(':tcd ' .. cd[choice])
+			vim.cmd(':Neotree action=focus')
 		end
 	end)
 end
